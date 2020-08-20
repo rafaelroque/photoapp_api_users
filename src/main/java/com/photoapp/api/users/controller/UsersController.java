47365ecb1b3354e38b51ffad1a779c942680816c
Rafael Roque <cinthiabastos@MacBook-Air-de-Cinthia-2.local>
2020-08-20 14:02:56 -0300

    Integration with Eureka Server

---
+++ src/main/java/com/photoapp/api/users/controller/UsersController.java
@@ -0,0 +1,15 @@
+package com.photoapp.api.users.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UsersController {
+
+	@GetMapping
+	public String status() {
+		return "Working";
+	}
+}
