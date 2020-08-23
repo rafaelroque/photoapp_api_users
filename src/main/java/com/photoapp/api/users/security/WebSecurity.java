30502d9d45d8b78c8f367868d34e6d3576113dee
Rafael Roque <cinthiabastos@MacBook-Air-de-Cinthia-2.local>
2020-08-23 15:22:13 -0300

    Allowing requests only from Zuul gateway

--- src/main/java/com/photoapp/api/users/security/WebSecurity.java
+++ src/main/java/com/photoapp/api/users/security/WebSecurity.java
@@ -1,29 +1,30 @@
 package com.photoapp.api.users.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import com.photoapp.api.users.util.Constants;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
 	
+	private Environment env;
+	
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
 		
 		http.csrf().disable();
+		http.headers().frameOptions().disable();
 		
 		http
 		.authorizeRequests()
-		.antMatchers("/users/**")
-		.permitAll()
-		.and()
-		.authorizeRequests()
-		.antMatchers("/h2-console/**")
-		.permitAll();
+		.antMatchers("/**")
+		.hasIpAddress(env.getProperty(Constants.GATEWAY_IP));
 		
-		http.headers().frameOptions().disable();
 	}
 
 }
---
+++ src/main/java/com/photoapp/api/users/util/Constants.java
@@ -0,0 +1,7 @@
+package com.photoapp.api.users.util;
+
+public class Constants {
+	
+	public static String GATEWAY_IP = "gateway.ip";
+
+}
