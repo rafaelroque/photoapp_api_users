cdc658608f82451b4cbf8aabbcbdf06e9e1d0538
Rafael Roque <cinthiabastos@MacBook-Air-de-Cinthia-2.local>
2020-08-23 15:14:33 -0300

    Encrypted user password

--- src/main/java/com/photoapp/api/users/service/UsersServiceImpl.java
+++ src/main/java/com/photoapp/api/users/service/UsersServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.UUID;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.photoapp.api.users.entity.User;
@@ -16,13 +17,17 @@ public class UsersServiceImpl implements UsersService {
 	
 	@Autowired
 	private UsersRepository repository;
+	
+	@Autowired
+	private BCryptPasswordEncoder encoder;
 
 	@Override
 	public UserTO createUser(UserTO to) {
 		to.setUserId(UUID.randomUUID().toString());
+		to.setEncryptedPassword(encoder.encode(to.getPassword()));
 		ModelMapper mapper = ModelMapperUtil.returnDefaultModelMapper();
 		User user = mapper.map(to, User.class);
-		user.setEncryptedPassword("test");
+		
 		repository.save(user);
 	
 		return ModelMapperUtil.returnDefaultModelMapper().map(user, UserTO.class);
